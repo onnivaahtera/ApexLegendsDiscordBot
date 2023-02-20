e3e9b0d8d543c505e623d69cfc0783d453ab4ad3
onnivaahtera <onni.vaahtera@gmail.com>
2023-02-20 18:38:49 +0200

    initial commit

---
+++ src/api.ts
@@ -0,0 +1,72 @@
+import { EmbedBuilder } from 'discord.js';
+import { config } from 'dotenv';
+config();
+
+export const getUseProfile = async (username: string) => {
+	const res = await fetch(
+		`https://api.mozambiquehe.re/bridge?auth=${process.env.API_KEY}&player=${username}&platform=PC`
+	);
+	const data = await res.json();
+
+	const message = new EmbedBuilder()
+		.setColor('DarkPurple')
+		.setTitle(data.global.name)
+		.setURL(`https://apexlegendsstatus.com/profile/PC/${username}`)
+		.setDescription(
+			`${
+				data.realtime.isOnline === 1
+					? ':green_circle: User is online'
+					: ':red_circle: User is offline'
+			}`
+		)
+		.addFields(
+			{
+				name: 'General',
+				value: `${
+					data.global.levelPrestige > 0
+						? `Level: ${data.global.level} (${data.global.toNextLevelPercent}%).\nPrestige ${data.global.levelPrestige}.`
+						: `Level: ${data.global.level} (${data.global.toNextLevelPercent}%).`
+				}`,
+				inline: true,
+			},
+			{ name: '\u200B', value: '\u200B', inline: true },
+			{
+				name: 'Ranked',
+				value: `Rank: ${data.global.rank.rankName} ${data.global.rank.rankDiv}\nRP: ${data.global.rank.rankScore}`,
+				inline: true,
+			},
+			{
+				name: 'Current legend',
+				value: `${data.legends.selected.LegendName}`,
+			}
+		);
+
+	return message;
+};
+
+export const getMap = async () => {
+	const res = await fetch(
+		`https://api.mozambiquehe.re/maprotation?auth=${process.env.API_KEY}&version=2`
+	);
+	const data = await res.json();
+
+	return `Current map is ${data.ranked.current.map} next map will be ${data.ranked.next.map} in ${data.ranked.current.remainingTimer}`;
+};
+
+export const getNews = async () => {
+	const res = await fetch(
+		`https://api.mozambiquehe.re/news?auth=${process.env.API_KEY}`
+	);
+	const data = await res.json();
+
+	const message = new EmbedBuilder()
+		.setColor('DarkPurple')
+		.setTitle(data[0].title)
+		.setURL(data[0].link)
+		.setDescription(data[0].short_desc)
+		.setImage(data[0].img);
+
+	return message;
+};
+
+getNews();
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import {
+	REST,
+	Routes,
+	Client,
+	GatewayIntentBits,
+	SlashCommandBuilder,
+} from 'discord.js';
+import { config } from 'dotenv';
+import { getMap, getNews, getUseProfile } from './api';
+config();
+
+const user = new SlashCommandBuilder()
+	.setName('user')
+	.setDescription('get user data')
+	.addStringOption((option) =>
+		option
+			.setName('username')
+			.setDescription('origin username')
+			.setRequired(true)
+	);
+
+const map = new SlashCommandBuilder()
+	.setName('map')
+	.setDescription('get current and next map');
+
+const store = new SlashCommandBuilder()
+	.setName('store')
+	.setDescription('Current store items');
+
+const news = new SlashCommandBuilder()
+	.setName('news')
+	.setDescription('get latest news');
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), {
+			body: [user, map, store, news],
+		});
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'user') {
+		const username = interaction.options.getString('username');
+		await interaction.reply({
+			embeds: [await getUseProfile(username!)],
+		});
+	}
+
+	if (interaction.commandName === 'map') {
+		await interaction.reply({
+			content: await getMap(),
+		});
+	}
+
+	if (interaction.commandName === 'store') {
+		await interaction.reply({
+			content:
+				"I'm lazy so here's the link: https://apexlegendsstatus.com/store",
+		});
+	}
+
+	if (interaction.commandName === 'news') {
+		await interaction.reply({
+			embeds: [await getNews()],
+		});
+	}
+});
+
+client.login(process.env.TOKEN);
